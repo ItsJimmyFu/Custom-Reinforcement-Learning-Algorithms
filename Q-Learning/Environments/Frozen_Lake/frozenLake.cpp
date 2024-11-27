4f49678ff6ebec4e93031036ef8c8912da99cecd
ItsJimmyFu <jimmyfu510@gmail.com>
2024-11-27 14:40:46 +1100

    Working q-learning algorithm

--- Q-Learning/Environments/Frozen_Lake/frozenLake.cpp
+++ Q-Learning/Environments/Frozen_Lake/frozenLake.cpp
@@ -8,14 +8,20 @@ using namespace std;
 
 class State {
     private:
+        
+    public:
         vector<array<int,2> > holesPos;
+        array<int,2> startPos;
         array<int,2> curPos;
         array<int,2> endPos;
         int gridHeight;
         int gridWidth;
-    public:
+        bool gameRunning;
+        string actions = "uldr";
+
         //Constructor to create a state from a string state
         State(string filePath) {
+            gameRunning = true;
             ifstream file(filePath);  // Open the file
             stringstream buffer;
             
@@ -31,7 +37,7 @@ class State {
             int curRow = 1;
             int curCol = 1;
             //Iterate through the state string
-            for (size_t i = 0; i < state.length(); ++i) {
+            for (size_t i = 0; i < state.length(); i++) {
                 //If reads the end of line, go to the position at the start of the next row
                 if(state[i] == '\n'){
                     curRow += 1;
@@ -42,6 +48,8 @@ class State {
                     if(state[i] == 'S'){
                         curPos[0] = curCol;
                         curPos[1] = curRow;
+                        startPos[0] = curCol;
+                        startPos[1] = curRow;
                     } else if (state[i] == 'H') {
                         // Add an array at the end using push_back
                         array<int, 2> holePos = {curCol, curRow};
@@ -57,46 +65,84 @@ class State {
             gridWidth = curCol-1;
         }
 
-        string makeAction(char direction) {
+        int getDimension(){
+            return gridHeight*gridWidth;
+        }
 
-            //Play the action
-            switch (tolower(direction)) {
-                case 'u':
-                    //Check if above the grid space
-                    if(curPos[1] == 1) {
-                        return "Invalid Action";
-                    }
-                    curPos[1] = curPos[1]-1;
-                    break;
-                case 'l':
-                    //Check if to the left of the grid space
-                    if(curPos[0] == 1) {
-                        return "Invalid Action";
-                    }
-                    curPos[0] = curPos[0]-1;
-                    break;
-                case 'd':
-                    //Check if below the grid space
-                    if(curPos[1] == gridHeight) {
-                        return "Invalid Action";
-                    }
-                    curPos[1] = curPos[1]+1;
-                    break;
-                case 'r':
-                    //Check if to the right of the grid space
-                    if(curPos[0] == gridWidth) {
-                        return "Invalid Action";
-                    }
-                    curPos[0] = curPos[0]+1;
-                    break;
+        int getActionSpace(){
+            return actions.size();
+        }
+
+        void reset(){
+            gameRunning = true;
+            curPos = startPos;
+        }
+
+        array<int,2> valToPos(int val){
+            int row = (val - 1) / gridWidth + 1;
+            int col = (val - 1) % gridWidth + 1;
+            return {col,row};
+        }
+
+        int posToVal(int col, int row){
+            return (row - 1) * gridWidth + col;
+        }
+
+        int getVal(){
+            return (curPos[1] - 1) * gridWidth + curPos[0];
+        }
+
+        //Get all the valid actions in the current state
+        string getValidActions(array<int,2> pos){
+            string validActions = "";
+
+             //Check if above the grid space
+            if(pos[1] > 1) {
+                validActions.push_back('u');
+            }
+            //Check if below the grid space
+            if(pos[1] < gridHeight) {
+                validActions.push_back('d');
+            }
+            //Check if to the left of the grid space
+            if(pos[0] > 1) {
+                validActions.push_back('l');
+            }
+            //Check if to the right of the grid space
+            if(pos[0] < gridWidth) {
+                validActions.push_back('r');
+            }
+            return validActions;
+        }
+
+        //Take the valid action and apply it to the state
+        string makeAction(char direction) {
+            if(gameRunning) {
+                //Play the action
+                switch (tolower(direction)) {
+                    case 'u':
+                        curPos[1] = curPos[1]-1;
+                        break;
+                    case 'l':
+                        curPos[0] = curPos[0]-1;
+                        break;
+                    case 'd':
+                        curPos[1] = curPos[1]+1;
+                        break;
+                    case 'r':
+                        curPos[0] = curPos[0]+1;
+                        break;
+                }
             }
 
             //Return feedback of action
             if(curPos == endPos){
+                gameRunning = false;
                 return "Finish";
             } else {
                 for (int n = 0; n < holesPos.size(); n++){
                     if(holesPos[n] == curPos){
+                        gameRunning = false;
                         return "Hole";
                     }
                 }
@@ -104,6 +150,7 @@ class State {
             }
         }
 
+        //Print the output of the state
         void printState(){
             cout << "Dimensions:" << gridWidth << " " << gridHeight << endl;
             cout << "Current Pos:" << curPos[0] << "," << curPos[1] << endl;
@@ -116,15 +163,3 @@ class State {
 
         }
 };
-
-int main() 
-{    
-    string filePath = "easy.txt";
-    State state(filePath);
-    state.printState();
-    cout << state.makeAction('r') << endl;
-    cout << state.makeAction('d') << endl;
-    cout << state.makeAction('r') << endl;
-
-    return 0;
-}
--- Q-Learning/qLearning.cpp
+++ Q-Learning/qLearning.cpp
@@ -1,9 +1,191 @@
 #include <iostream>  // Include the iostream library for input/output
+#include "Environments/Frozen_Lake/frozenLake.cpp"
+#include <cmath> 
+#include <random>
+#include <string>
+#include <vector>
 
-int main() {
-    // Output "Hello, World!" to the console
-    std::cout << "Hello, World!" << std::endl;
+double** trainAgent(float learningRate, int nTraining, int nEvaluation, State state, int maxSteps,
+float gamma, float maxEpsilon, float minEpsilon, float decayRate, unsigned int seed);
+vector<char> evaluateAgent(State state, double** qTable, int maxSteps);
+
+int main() 
+{   
+    //Load environment 
+    string environmentPath = "Environments/Frozen_lake/easy.txt";
+    State state(environmentPath);
+    int qTableRows = state.getDimension();
+    int qTableCols = state.getActionSpace();
+
+    //Parameters
+    float learningRate = 0.7;
+    int nTraining = 10000;
+    int nEvaluation = 100;
+    int maxSteps = 99;
+    float gamma  = 0.95;
+    float maxEpsilon = 1.0;           
+    float minEpsilon = 0.05;           
+    float decayRate = 0.0005;
+    unsigned int seed = 12345;
     
-    // Return 0 to indicate successful execution
+    double** qTable = trainAgent(learningRate, nTraining, nEvaluation,state,maxSteps,gamma,maxEpsilon,minEpsilon,decayRate,seed);
+    
+    cout << "Final Q Table\n";
+    for (int i = 0; i < qTableRows; ++i) {
+        cout << "State " << i << ":";
+        for (int j = 0; j < qTableCols; ++j) {
+            cout << qTable[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    vector<char> solution = evaluateAgent(state,qTable,100);
+    
+    cout << "Solution actions: ";
+    for (size_t i = 0; i < solution.size(); ++i) {
+        cout << solution[i] << " ";
+    }
+    cout << endl;
+
     return 0;
+}
+
+vector<char> evaluateAgent(State state, double** qTable, int maxSteps){
+    vector<char> solActionSet;
+    int qTableRows = state.getDimension();
+    int qTableCols = state.getActionSpace();
+
+    for (int step = 0; step < maxSteps; step++){
+        char action;
+        int stateIndex = state.getVal()-1;
+
+        //Find the best action in the q-table at the corresponding state
+        int maxIndex = -1;
+        double maxVal = -100;
+            
+        //Find the max value in a state
+        for (int n = 0; n < qTableCols; n++){
+            if(qTable[stateIndex][n] > maxVal){
+                maxVal = qTable[stateIndex][n];
+                maxIndex = n;
+            }
+        }
+        action = state.actions[maxIndex];
+        solActionSet.push_back(action);
+
+        //Apply the best action to the state
+        state.makeAction(action);
+     
+        if(!state.gameRunning) {
+            break;
+        }
+    }
+    return solActionSet;
+}
+
+double** trainAgent(float learningRate, int nTraining, int nEvaluation, State state, int maxSteps,
+float gamma, float maxEpsilon, float minEpsilon, float decayRate, unsigned int seed){
+        
+    //Random 
+    mt19937 gen(seed);
+    //Random distribution for exploitative vs exploration probability
+    uniform_real_distribution<float> dis(0.0f, 1.0f);
+    //Random distribution for 2, 3 and 4 valid action sets
+    std::uniform_int_distribution<int> validActions4(0, 3);
+    std::uniform_int_distribution<int> validActions3(0, 2);
+    std::uniform_int_distribution<int> validActions2(0, 1);
+  
+
+    //Create the q-table by allocating it to memory
+    int qTableRows = state.getDimension();
+    int qTableCols = state.getActionSpace();
+    double** qTable = new double*[qTableRows];
+    for (int i = 0; i < qTableRows; ++i) {
+        qTable[i] = new double[qTableCols]();
+    }
+    
+    //Initialise values inside the q-table by setting it to 0
+    for (int i = 0; i < qTableRows; ++i) {
+        for (int j = 0; j < qTableCols; ++j) {
+            qTable[i][j] = 0;
+        }
+    }
+
+    //Iterate through each episode
+    for (int episodeN = 0; episodeN < nTraining; episodeN++) {
+        //Set the epsilon value which decreases over episodes
+        double epsilon = minEpsilon + (maxEpsilon - minEpsilon) * pow(M_E, (-decayRate * episodeN));
+
+        //Reset the initial environment state
+        state.reset();
+
+        //Iterate through steps
+        for (int step = 0; step < maxSteps; step++){
+            //Get the action based on a random value
+            float value = dis(gen);
+            char action;
+            int stateIndex = state.getVal()-1;
+            //Exploit
+            if(value > epsilon) {
+                //Find the best action in the q-table at the corresponding state
+                int maxIndex = -1;
+                double maxVal = -100;
+
+                string validMoves = state.getValidActions(state.curPos);
+                string possibleMoves = "uldr";
+                 
+                //Find the max value in a state
+                for (int n = 0; n < qTableCols; n++){
+                    //Check if move is a valid move
+                    if(validMoves.find(possibleMoves[n]) == string::npos){
+                        continue;
+                    }
+                    if(qTable[stateIndex][n] > maxVal){
+                        maxVal = qTable[stateIndex][n];
+                        maxIndex = n;
+                    }
+                }
+                action = state.actions[maxIndex];
+            }
+            //Explore
+            else {
+                //Pick a random valid move
+                string moves = state.getValidActions(state.curPos);
+                if(moves.size() == 2) {
+                    action = moves[validActions2(gen)];
+                } else if(moves.size() == 3) {
+                    action = moves[validActions3(gen)];
+                } else if (moves.size() == 4) {
+                    action = moves[validActions4(gen)];
+                } else {
+                    cout << "Invalid actions";
+                }
+            }
+
+            //Get the reward value of the new state
+            string result = state.makeAction(action);
+            int newStateIndex = state.getVal()-1;
+            int reward = 0;
+            if(result == "Finish") {
+                reward = 1;
+            } 
+
+            //Get the best value of the new state
+            double newStateMaxVal = -1;
+
+            for (int n = 0; n < qTableCols; n++){
+                if(qTable[newStateIndex][n] > newStateMaxVal){
+                    newStateMaxVal = qTable[newStateIndex][n];
+                }
+            }
+
+            int actionIndex = state.actions.find(action);
+
+            qTable[stateIndex][actionIndex] = qTable[stateIndex][actionIndex] + learningRate * (reward + gamma * newStateMaxVal - qTable[stateIndex][actionIndex]);
+            if(!state.gameRunning) {
+                break;
+            }
+        }
+    } 
+    return qTable;
 }
